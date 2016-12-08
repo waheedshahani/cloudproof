553fc3387a92277c667d7b6f7617bb3cc5f99c03
WAHEED SHAHANI <waheed.shahani@gmail.com>
2016-12-08 15:16:10 -0800

    working on hashing and attestation

--- cloudproof/client/rpcclient.py
+++ cloudproof/client/rpcclient.py
@@ -1,5 +1,5 @@
-import hashlib
 import cPickle
+import hashlib
 import xmlrpclib
 import base64
 from Crypto.PublicKey import RSA
@@ -29,22 +29,35 @@ def encryptAndEncode(plainText,key):
     key=AES.new(key, AES.MODE_CTR, counter=lambda: ctr)
     encrypted=key.encrypt(plainText)
     b64=base64.b64encode(encrypted)
-    print b64
-    print ctr
+    return b64
+def hashAndSign(blockId,encryptedEncodedContent):
+    global keyDistributor
+    signingKey=cPickle.loads(keyDistributor.getSigningKey(1,user))
+    print signingKey
+    hashOfContent= hashlib.md5(encryptedEncodedContent)
+    HexHash=hashOfContent.hexdigest()
+    print HexHash
+    HexHash="SHAHANI WAHEED ALI"
+    #hexstr=str.encode(HexHash)
+    #b64Hash=base64.b64encode(hexstr)
+    type(HexHash)    
+    signed=signingKey.encrypt(HexHash,32)[0]
+    b64=base64.b64encode(signed)
+    print "Base 64%s" %b64
+    return "test"
     return b64
 def decodeAndDecrypt(base64cipher,key):
-     print base64cipher
      base64decoded=base64.b64decode(base64cipher)
      key=AES.new(key, AES.MODE_CTR, counter=lambda: ctr)
      decrypted=key.decrypt(base64decoded)
      print ctr
      print ("Decrypted:%s" %decrypted)
      return decrypted
-def put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation):
+def put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation,hashSign):
     global cloudStorage
     hash_object = hashlib.md5(content)
     New_Hash=hash_object.hexdigest()
-    cloudStorage.put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation)
+    cloudStorage.put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation,hashSign)
 def get(block_Id):
     global cloudStorage
     [block_Version,content,cloud_get_attestation]=cloudStorage.get(block_Id)
@@ -64,6 +77,9 @@ secretKey=cPickle.loads(keyDistributor.getSecretKey(1,'u1'))
 #pub_key=cPickle.loads(keyDistributor.getKey(1,'r',user))
 #encryptedContent=encryptAndEncode("WAHEED ENCRYPTED",pub_key)
 #put(block_id,block_version,encryptedContent)
+content1=encryptAndEncode("Game of Thrones",secretKey)
+content2=encryptAndEncode("Narcos",secretKey)
+content3=encryptAndEncode("Breaking Bad",secretKey)
 if rw == 'w':
     #pri_key=cPickle.loads(keyDistributor.getKey(1,'w',user))
     [block_version_no,cipherContent,cloud_get_attestation]=get(block_id)
@@ -78,9 +94,10 @@ if rw == 'w':
     print ("Current Content:%s" %decodeAndDecrypt(cipherContent,secretKey))
     print ("Cloud Get Attestation:%s" %cloud_get_attestation)
 else:
-    put('client_put_attestation',1,' ',100,'new Hash',encryptAndEncode("Game of Thrones",secretKey),'Hashed_signed_attestation')
-    put('client_put_attestation',2,' ',100,'new Hash',encryptAndEncode("Narcos",secretKey),'Hashed_signed_attestation')
-    put('client_put_attestation',3,' ',100,'new Hash',encryptAndEncode("Breaking Bad",secretKey),'Hashed_signed_attestation')
+    hashAndSign(1,content1)
+    put('client_put_attestation',1,' ',100,'new Hash',content1,'Hashed_signed_attestation',hashAndSign(1,content1))
+#    put('client_put_attestation',2,' ',100,'new Hash',content2,'Hashed_signed_attestation',hashAndSign(2,content2))
+#    put('client_put_attestation',3,' ',100,'new Hash',content3,'Hashed_signed_attestation',hashAndSign(3,content3))
 
     #put(1,100,encryptAndEncode("Game of Thrones",pub_key),"CPA")
     #put(2,100,encryptAndEncode("Narcos",pub_key),"CPA")
--- cloudproof/server/rpcserver.py
+++ cloudproof/server/rpcserver.py
@@ -1,23 +1,54 @@
+import base64
+import cPickle
 import hashlib
 import xmlrpclib
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 #initializing 5 blocks with initial values as null
 #storage={1:[],2:[],3:[],4:[],5:[]}
+keyDistributor = xmlrpclib.ServerProxy("http://localhost:8001/", allow_none=True)
 storage={}
 cloud_get_attestation='This is cloud put attestation'
-def put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation):
+
+def put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation,hashSign):
     print("BLOCK ID: %s" %block_Id)
 #    print("Content: %s" %content)
     print("Key Block Version: %s" %new_Version_No)
     hash_object = hashlib.md5(content)
-    print(hash_object.hexdigest())
     if hashlib.md5(content).hexdigest() in New_Hash:
         print "Hashes match"
+    print hashSign
+    verifySignature(block_Id,content,hashSign)
     storage[block_Id]=[new_Version_No,content]
 def get(block_Id):
     [block_Version_No,content]=storage[block_Id]
     return [block_Version_No,content,cloud_get_attestation]
 
+def verifySignature(block_Id,encryptedEncodedContent,hashSign):
+    global keyDistributor
+    #First decoding and decrypting hashsign
+    print  "Hash Sign:%s" %hashSign
+    verificationKey=cPickle.loads(keyDistributor.getPublicKey(block_Id,'cloud'))
+    print "here2"
+    decrptb64Hash=verificationKey.decrypt(hashSign)
+    print decrptb64Hash
+#    b64=base64.b64decode(hashSign)
+#    print b64   
+#    print "Decoded hashsign %s" %str.decode(b64)
+#    decrptb64Hash=verificationKey.decrypt(b64)
+#    b64=base64.b64encode(decrptb64Hash)
+#    print b64
+    # calculating hashsign on content we received
+#    contentHash=hashlib.md5(encryptedEncodedContent)
+#    HexHash=contentHash.hexdigest()
+#    b64new=base64.b64encode(hashSign)
+#    print b64new
+#    print "Here3"
+#    if  b64 == b64new:
+#        print "Integerity works"
+#        return
+ #   print "Integrity failed"
+    
+    
 server = SimpleXMLRPCServer(("localhost", 8000), allow_none=True)
 print ("Listening on port 8000...")
 server.register_function(put, "put")
