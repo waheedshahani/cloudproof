fc1416b83cd29ceb20966bccb343e7552b9db7d9
WAHEED SHAHANI <waheed.shahani@gmail.com>
2016-12-11 15:49:19 -0800

    Worked on attestations - client and cloud put attestation working.

--- cloudproof/client/rpcclient.py
+++ cloudproof/client/rpcclient.py
@@ -1,3 +1,6 @@
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Hash import SHA
 import cPickle
 import hashlib
 import xmlrpclib
@@ -8,102 +11,121 @@ from Crypto.Util import Counter
 import os
 cloudStorage = xmlrpclib.ServerProxy("http://localhost:8000/", allow_none=True)
 keyDistributor = xmlrpclib.ServerProxy("http://localhost:8001/", allow_none=True)
-#key=cPickle.loads(keyDistributor.getKey(1,'r','u1'))
-ctr=os.urandom(16)
-#cipher = AES.new(key, AES.MODE_CTR, counter=lambda: ctr)
-#ct = cipher.encrypt("asdk")
-#print (str(ct))
-#print ("decrypted")
+key_block_Version_No_inti=[os.urandom(16),os.urandom(16),os.urandom(16),os.urandom(16)]
 
-#dcipher = AES.new(key, AES.MODE_CTR, counter=lambda: ctr)
-#print (dcipher.decrypt(ct))
-#exit()
-
+def picklethis(object):
+    with open('temp','w') as temp:
+        cPickle.dump(object,temp)
+    f=open('temp','r').read()
+    return f
+def unpicklethis(object):
+    return cPickle.loads(object)
 def readKey():
     f=open('key.pem','r')
     key=RSA.importKey(f.read())
     f.close()
     return key
-
-def encryptAndEncode(plainText,key):
-    key=AES.new(key, AES.MODE_CTR, counter=lambda: ctr)
+def encryptAndEncode(plainText,key,key_block_Version_No):
+    key=AES.new(key, AES.MODE_CTR, counter=lambda: key_block_Version_No)
     encrypted=key.encrypt(plainText)
     b64=base64.b64encode(encrypted)
     return b64
-def hashAndSign(blockId,encryptedEncodedContent):
+#This will return hash of content. passed as New_Hash to cloud. 
+def hash(encryptedEncodedContent):
+    h = SHA256.new(encryptedEncodedContent).hexdigest()
+    return picklethis(h)
+def hashAndSign(blockId,encryptedEncodedContent,user):
     global keyDistributor
-    signingKey=cPickle.loads(keyDistributor.getSigningKey(1,user))
-    print signingKey
-    hashOfContent= hashlib.md5(encryptedEncodedContent)
-    HexHash=hashOfContent.hexdigest()
-    print HexHash
-    HexHash="SHAHANI WAHEED ALI"
-    #hexstr=str.encode(HexHash)
-    #b64Hash=base64.b64encode(hexstr)
-    type(HexHash)    
-    signed=signingKey.encrypt(HexHash,32)[0]
-    b64=base64.b64encode(signed)
-    print "Base 64%s" %b64
-    return "test"
-    return b64
-def decodeAndDecrypt(base64cipher,key):
+    key=keyDistributor.getSigningKey(blockId,user)
+    key=RSA.importKey(key)
+    h = SHA256.new(encryptedEncodedContent).hexdigest()
+    sig = key.sign(h,'')
+    return picklethis(sig)
+def createClientPutAttestation(block_Id,key_block_Version_No,new_Version_No,new_Hash,encryptedEncodedContent,user):
+    global keyDistributor
+    key=keyDistributor.getSigningKey(block_Id,user)
+    key=RSA.importKey(key)
+    concat=str(block_Id)+str(key_block_Version_No)+str(new_Version_No)+new_Hash+encryptedEncodedContent
+    h = SHA256.new(concat).hexdigest()
+    sign=key.sign(h,'')
+    return [picklethis(sign),h]
+def decodeAndDecrypt(base64cipher,key,key_block_Version_No):
      base64decoded=base64.b64decode(base64cipher)
-     key=AES.new(key, AES.MODE_CTR, counter=lambda: ctr)
+     key=AES.new(key, AES.MODE_CTR, counter=lambda: key_block_Version_No)
      decrypted=key.decrypt(base64decoded)
-     print ctr
      print ("Decrypted:%s" %decrypted)
      return decrypted
 def put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation,hashSign):
     global cloudStorage
-    hash_object = hashlib.md5(content)
-    New_Hash=hash_object.hexdigest()
-    cloudStorage.put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation,hashSign)
+#    hash_object = hashlib.md5(content)
+#    New_Hash=hash_object.hexdigest()
+    key_block_Version_No=picklethis(key_block_Version_No)
+    cloudReply=cloudStorage.put(client_Put_Attest,block_Id,key_block_Version_No,new_Version_No,New_Hash,content,hashedSignedAttestation,hashSign)
+    return cloudReply
 def get(block_Id):
     global cloudStorage
-    [block_Version,content,cloud_get_attestation]=cloudStorage.get(block_Id)
-    return [block_Version,content,cloud_get_attestation]
-   
+    [block_Version_No,content,hashSign,key_block_Version_No,cloud_Get_Attest]=cloudStorage.get(block_Id)
+    key_block_Version_No=unpicklethis(key_block_Version_No)
+    return [block_Version_No,content,hashSign,key_block_Version_No]
+#Verify integiry of the block while reading a block.
+def verifyIntegrity(block_Id,encryptedEncodedContent,hashSign):
+    global keyDistributor
+    key=keyDistributor.getPublicKey(block_Id,'u1')
+    key=RSA.importKey(key)
+    hashSign=unpicklethis(hashSign)
+    h = SHA256.new(encryptedEncodedContent).hexdigest()
+    return verifySignature(key,hashSign,h)
+#    if (key.verify(h,hashSign)):
+#        return 1
+#    else:
+#        return 0
+def verifySignature(key,signedHash,hash):
+    return key.verify(hash,signedHash)
 pub_key=()
 pri_key=()
-user='u1'
-block_id=1
-#rw='w'
-#user=raw_input("Enter user name u1,u2,u3?:")
-#block_id=raw_input("Enter block ID:")
-rw=raw_input("Read or Write?:")
+#user='u1'
+block_Id=1
+rw='r'
+key_block1_ver_no_init=os.urandom(16)
+key_block2_ver_no_init=os.urandom(16)
+key_block3_ver_no_init=os.urandom(16)
 secretKey=cPickle.loads(keyDistributor.getSecretKey(1,'u1'))
-#secretKey=AES.new(key, AES.MODE_CTR, counter=lambda: ctr)
+content1=encryptAndEncode("Game of Thrones",unpicklethis(keyDistributor.getSecretKey(1,'u1')),key_block1_ver_no_init)
+content2=encryptAndEncode("Narcos",unpicklethis(keyDistributor.getSecretKey(2,'u2')),key_block2_ver_no_init)
+content3=encryptAndEncode("Breaking Bad",unpicklethis(keyDistributor.getSecretKey(3,'u3')),key_block3_ver_no_init)
+while True:
+    rw=raw_input("Read or Write?:")
+    if rw == 'q':
+        break
 
-#pub_key=cPickle.loads(keyDistributor.getKey(1,'r',user))
-#encryptedContent=encryptAndEncode("WAHEED ENCRYPTED",pub_key)
-#put(block_id,block_version,encryptedContent)
-content1=encryptAndEncode("Game of Thrones",secretKey)
-content2=encryptAndEncode("Narcos",secretKey)
-content3=encryptAndEncode("Breaking Bad",secretKey)
-if rw == 'w':
-    #pri_key=cPickle.loads(keyDistributor.getKey(1,'w',user))
-    [block_version_no,cipherContent,cloud_get_attestation]=get(block_id)
-    print ("Current Version:%s" %block_version_no)
-    print ("Current Content:%s" %decodeAndDecrypt(cipherContent,secretKey))
-    print ("Cloud Get Attestation:%s" %cloud_get_attestation)
-    newContent=raw_input("Lets Modify content to?:")
-    encryptedContent=encryptAndEncode(newContent,secretKey)
-    put('client_put_attestation',block_id,' ',block_version_no+1,'new Hash',encryptedContent,'Hashed_signed_attestation')
-    [block_version_no,cipherContent,cloud_get_attestation]=get(block_id)
-    print ("Current Version:%s" %block_version_no)
-    print ("Current Content:%s" %decodeAndDecrypt(cipherContent,secretKey))
-    print ("Cloud Get Attestation:%s" %cloud_get_attestation)
-else:
-    hashAndSign(1,content1)
-    put('client_put_attestation',1,' ',100,'new Hash',content1,'Hashed_signed_attestation',hashAndSign(1,content1))
-#    put('client_put_attestation',2,' ',100,'new Hash',content2,'Hashed_signed_attestation',hashAndSign(2,content2))
-#    put('client_put_attestation',3,' ',100,'new Hash',content3,'Hashed_signed_attestation',hashAndSign(3,content3))
+    if rw == 'w':
+        [block_version_no,cipherContent,hashSign,key_block_Version_No]=get(block_Id)
+        print ("Current Version:%s" %block_version_no)
+        print ("Current Content:%s" %decodeAndDecrypt(cipherContent,secretKey,key_block_Version_No))
+        if (verifyIntegrity(block_Id,cipherContent,hashSign)):
+            print ("received integrity passed")
+            newContent=raw_input("Lets Modify content to?:")
+            content1=encryptAndEncode(newContent,secretKey,key_block_Version_No)
+            print ("Encoded content1%s" %content1)
+            put('client_put_attestation',block_Id,key_block_Version_No,block_version_no+1,'new Hash',content1,'Hashed_signed_attestation',hashAndSign(1,content1,'u1'))
+            [block_version_no,cipherContent,hashSign,key_block_Version_No]=get(block_Id)
+            print ("Updated Version:%s" %block_version_no)
+            print ("Updated Content:%s" %decodeAndDecrypt(cipherContent,secretKey,key_block_Version_No))
+            print ()
+        else:
+            print ("Received integrity failed: I hate Cloud.")
+    elif rw == 'r':
+        [clientputattestation1,hashOfElements]=createClientPutAttestation(1,picklethis(key_block1_ver_no_init),100,hash(content1),content1,'u1')
+        new_Hash1=hash(content1)
+#        print ("New Hash:%s" %new_Hash1)
+#        clientputattestation2=createClientPutAttestation(2,key_block2_ver_no_init,100,hash(content2),content2,'u2')
+#        new_Hash2=hash(content2)
+#        clientputattestation3=createClientPutAttestation(3,key_block3_ver_no_init,100,hash(content3),content3,'u3')
+#        new_Hash3=hash(content3)
+        returnValue=put(clientputattestation1,1,key_block1_ver_no_init,100,new_Hash1,content1,clientputattestation1,hashAndSign(1,content1,'u1'))
+        cloudPublicKey=unpicklethis(cloudStorage.getPublicKey())
+        if verifySignature(cloudPublicKey,unpicklethis(returnValue),hashOfElements):
+            print ("Cloud Put attestation looks good. I'll store it for later use")
+#        put(clientputattestation2,2,key_block2_ver_no_init,100,hash(content2),content2,clientputattestation2,hashAndSign(2,content2,'u2'))
+#        put(clientputattestation2,3,key_block3_ver_no_init,100,hash(content3),content3,clientputattestation3,hashAndSign(3,content3,'u3'))
 
-    #put(1,100,encryptAndEncode("Game of Thrones",pub_key),"CPA")
-    #put(2,100,encryptAndEncode("Narcos",pub_key),"CPA")
-    #put(3,100,encryptAndEncode("Breaking Bad",pub_key),"CPA")
-    #put(1,101,encryptAndEncode("Game of Thrones",pub_key),"CPA")
-
-#print ("Decrypting data")
-#for i in range(1,4):
-#   print (get(i))
---
+++ cloudproof/keydistributor/keyDistributor.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+import os
+import base64
+import cPickle
+import xmlrpclib
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from Crypto.PublicKey import RSA
+from Crypto import Random
+#This code generates keys (public & private) for each block. 
+totalKeysNeeded=6
+keyGenerateFlag=0
+#src_data = 'To be, or not to be - that is the question.'
+#print `src_data`
+acl={1:{'r':['u1','u2','u3','cloud'],'w':['u1']},\
+     2:{'r':['u2','u1','u3','cloud'],'w':['u2']},\
+     3:{'r':['u3','u1','u2','cloud'],'w':['u3']},\
+     4:{'r':['u4','u5','cloud'],'w':['u4']},\
+     5:{'r':['u5','u4','cloud'],'w':['u5']},}
+#Generating secret keys for CBC counter mode cryptography.
+blockKeys={1:os.urandom(32),2:os.urandom(32),3:os.urandom(32),4:os.urandom(32),5:os.urandom(32)}
+#This method receives any object and returns pickled version which can be serialized. 
+#on receiver end this should be received using cPickle.loads and gives back object.....
+def picklethis(object):
+    with open('temp','w') as temp:
+        cPickle.dump(object,temp)
+    f=open('temp','r').read()
+    return f
+def unpicklethis(object):
+    return cPickle.loads(object)
+#This is secret key. any user with read access or write access can get this key. This is only used for en(de)crypting data.
+def getSecretKey(block_Id,user):
+     with open('temp','w') as temp:
+        cPickle.dump(blockKeys[block_Id],temp)
+     f=open('temp','r').read()
+     return f
+
+#This key pair is used signing and verification. private key is called signing key and public key is called verification key. 
+def getSigningKey(block_Id,user):
+     dict1= acl[block_Id]
+     userList=dict1['w']
+     if user not in userList:
+         return 0
+     f=open('%s.pri_key' %block_Id,'r').read()
+     print ("Signing key %s.pri_key returned" %block_Id)
+     return f
+def getPublicKey(block_Id,user):
+     dict1= acl[block_Id] #dict1 is dictionary
+     #getting list of user for rw of particular block.
+     userList=dict1['r']
+     if user not in userList:
+         print "**User not in list**"
+         return 0
+     f=open('%s.pub_key' %block_Id,'r').read()
+     print ("Public key %s.pub_key returned" %block_Id)
+     return f
+
+def getKey(block_Id,rw,user):
+     dict1= acl[block_Id] #dict1 is dictionary
+     #getting list of user for rw of particular block.
+     userList=dict1[rw]
+     if user not in userList:
+         return 0
+     if rw == 'r':
+         f=open('%s.pub_key' %block_Id,'r').read()
+     elif rw == 'w':
+         f=open('%s.pri_key' %block_Id,'r').read() 
+     rsakey=RSA.importKey(f)
+     print rsakey
+     #Converting object into character stream for serialization
+     with open('temp','w') as temp:
+         cPickle.dump(rsakey,temp) 
+     f=open('temp','r').read()
+#     print (f)
+     #b64key=base64.b64encode(f)
+     #f.close()
+     #print rsakey
+     return f     
+#     return b64key
+
+
+if keyGenerateFlag==1:
+    for i in range (0,totalKeysNeeded):
+        random_generator = Random.new().read
+        key = RSA.generate(1024, random_generator)
+        f=open('%d.pri_key' %i,'w')
+        f.write(key.exportKey('PEM'))
+        f.close()
+        f=open('%d.pub_key' %i,'w')
+        f.write(key.publickey().exportKey('PEM'))
+    print ("Keys Generated")
+
+keyServer=SimpleXMLRPCServer(("localhost", 8001), allow_none=True)
+print ("Listening on port 8001...")
+keyServer.register_function(getKey, "getKey")
+keyServer.register_function(getSecretKey,"getSecretKey")
+keyServer.register_function(getSigningKey,"getSigningKey")
+keyServer.register_function(getPublicKey,"getPublicKey")
+keyServer.serve_forever()
+
+
+#f=open('key.pem','r')
+#key=RSA.importKey(f.read())
+#f.close()
+#pub_key = key.publickey()
+#print 'Public key', pub_key
+
+#enc_data = pub_key.encrypt(src_data, 32)[0]
+#print `enc_data`
+
+#dec_data = key.decrypt(enc_data)
+#print `dec_data`
+
+#publickey = key.exportKey('DER')
+#privatekey = key.publickey().exportKey('DER')
+#f=open('publicKey.pem','w')
+#f.write(publickey)
+#f.close()
+#f=open('privateKey.pem','w')
+#f.write(privatekey)
+#f.close()
+
